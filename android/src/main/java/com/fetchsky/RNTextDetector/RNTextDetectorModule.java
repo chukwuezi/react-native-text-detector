6b81ef37b47a8f0161adc9bd64989d37e845f7dd
Chinedum <chinedum@Chriss-MBP.home>
2019-03-11 14:40:19 -0700

    import firbaseapp

--- android/src/main/java/com/fetchsky/RNTextDetector/RNTextDetectorModule.java
+++ android/src/main/java/com/fetchsky/RNTextDetector/RNTextDetectorModule.java
@@ -16,6 +16,7 @@ import com.facebook.react.bridge.WritableMap;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
 import com.google.firebase.ml.vision.FirebaseVision;
 import com.google.firebase.ml.vision.common.FirebaseVisionImage;
 import com.google.firebase.ml.vision.text.FirebaseVisionText;
